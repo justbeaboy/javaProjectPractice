e9820447ceb0bce3b68dace63a5f593a6fd80aa4
zhangyanbao <zhangyanbao@weilaisihang.com>
2018-08-29 10:46:31 +0800

    IDEA整合GitHub测试

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: zhangyanbao
+ * @date: 2018-08-28 16:33
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("jjdsjf");
+    }
+}
